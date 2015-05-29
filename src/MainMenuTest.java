0a2c773eee996ec4e2eafb6a1e2d3af040be8666
Hannah Waxman <hannah.waxman8@gmail.com>
2015-05-29 14:27:15 +0100

    Refactored main menu tests

--- src/MainMenuTest.java
+++ src/MainMenuTest.java
@@ -11,28 +11,25 @@ public class MainMenuTest {
     @Before
     public void setUp() throws Exception {
         menu = new MainMenu();
+        menu.addOption("1. List Books");
+        String userInput = "1";
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
     }
 
     @Test
     public void canAddOptionsToMenu() {
-        menu.setMenuSize();
-        assertEquals(menu.getMenuSize(), 0);
-        menu.addOption("1. List Books");
-        menu.setMenuSize();
         assertEquals(menu.getMenuSize(), 1);
+        menu.addOption("2. Quit");
+        assertEquals(menu.getMenuSize(), 2);
     }
 
     @Test
-    public void listsMenuOptions() {
-        menu.addOption("1. List Books");
+    public void displaysMenuOptions() {
         assertEquals(menu.displayMenu(), "1. List Books ");
     }
 
     @Test
     public void setsUserChoice() {
-        menu.addOption("1. List Books");
-        String userInput = "1";
-        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
         assertEquals(menu.getUserChoice(), 0);
         menu.setUserChoice();
         assertEquals(menu.getUserChoice(), 1);
@@ -40,18 +37,12 @@ public class MainMenuTest {
 
     @Test
     public void validChoiceIfWithinMenuBounds() {
-        menu.addOption("1. List Books");
-        menu.setMenuSize();
-        String userInput = "1";
-        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
         menu.setUserChoice();
         assertEquals(menu.validMenuChoice(), true);
     }
 
     @Test
     public void invalidChoiceIfOutOfMenuBounds() {
-        menu.addOption("1. List Books");
-        menu.setMenuSize();
         String userInput = "2";
         System.setIn(new ByteArrayInputStream(userInput.getBytes()));
         menu.setUserChoice();
